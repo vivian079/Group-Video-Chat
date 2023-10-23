f9e2d6a4cd030e65cdd9253a926bd7525a3ccffc
vivian079 <102835705+vivian079@users.noreply.github.com>
2023-10-22 22:46:20 -0400

    Add files via upload

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import IntroductionPage from "./IntroductionPage/IntroductionPage";
+import JoinRoomPage from "./JoinRoomPage/JoinRoomPage";
+import RoomPage from "./RoomPage/RoomPage";
+import { connectWithSocketIOServer } from "./utils/wss";
 
-import './App.css';
+import "./App.css";
 
 function App() {
+  useEffect(() => {
+    connectWithSocketIOServer();
+  }, []);
+
   return (
-    <div className="App">
-      Hello here is react application
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/join-room">
+          <JoinRoomPage />
+        </Route>
+        <Route path="/room">
+          <RoomPage />
+        </Route>
+        <Route path="/">
+          <IntroductionPage />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
